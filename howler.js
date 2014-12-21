cecabcc111e5ea772037a1db38a421ad54a17e53
stealthycoin <john.w.carlyle@gmail.com>
2014-12-21 00:08:33 -0800

    initial version

---
+++ howler.js
@@ -0,0 +1,53 @@
+var howler = (function() {
+
+    var counter, target;
+    var batching = false;
+    var callback;
+    var templates = {};
+
+    return {
+        fetch: function(name) {
+            if (name in templates) {
+                return templates[name];
+            }
+            else {
+                console.log(name + " not loaded yet.");
+            }
+        },
+
+        batch: function(names, cb) {
+            if (batching) {
+                console.log("Can only load one batch at a time.");
+                return;
+            }
+
+            batching = true;
+            callback = cb;
+            counter = templates.length || 0;
+            target = counter + names.length;
+            for (var i in names) {
+                howler.load(names[i]);
+            }
+
+        },
+
+        load: function(name) {
+            $.ajax({
+                url: "/static/tmpl/" + name,
+                type: "GET",
+                cached: true,
+            }).done(function(data) {
+                templates[name] = Handlebars.compile(data);
+                if (batching) {
+                    counter++;
+                    if (counter === target) {
+                        batching = false;
+                        callback();
+                    }
+                }
+            }).fail(function(xhr) {
+                console.log(xhr);
+            });
+        }
+    };
+})();
